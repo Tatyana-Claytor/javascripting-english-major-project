4b54c866d0a73115cd691d0bf5bdc81e537fca31
Tatyana-Claytor <55713169+Tatyana-Claytor@users.noreply.github.com>
2020-01-24 18:16:57 -0500

    tiprate

--- scripts.js
+++ scripts.js
@@ -4,3 +4,9 @@ while ( i < 4 ) {
   $("#response").append("<br />" + i);
   i = i + 1;
 }
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  let tipAmount;
+  tipAmount = tipRate * total;
+  $("#response").html("Your tip is $" + tipAmount);
+};
